694c41e8ebdfe1df5e9b5dc5389f0f4fccebe331
Jefferson Silva <fj_adm@hotmail.com>
2025-09-22 22:18:26 -0300

    AULA 22 - Text Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// Components
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
   template: `
     <!-- <router-outlet></router-outlet> -->
+    <!-- <app-new-component /> -->
     <h1>Curso de Angular</h1>
-    <div class="theme-red">
-      <app-new-component />
-    </div>
+    <app-template-binding />
   `,
 })
 export class AppComponent {}
